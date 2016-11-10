45854ba86683dc26bfc42bf77026721d7997f33a
mohsen0571 <nasimbahar73@yahoo.com>
2016-11-10 05:31:45 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {287272030},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
